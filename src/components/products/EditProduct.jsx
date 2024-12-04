0eca3507f681dd93716b195690cdc35feb43ae86
nguyentb1148 <nguyentbgcd201888@fpt.edu.vn>
2024-12-04 14:48:13 +0700

    Edit Product(not finished yet)

--- src/components/products/EditProduct.jsx
+++ src/components/products/EditProduct.jsx
@@ -1,44 +1,407 @@
-import React, { useEffect, useState } from 'react';
-import { getProductById } from "../../services/api/ProductApi.jsx";
+import React, { useEffect, useState, useRef } from "react";
+import { getProductById, updateProduct } from "../../services/api/ProductApi.jsx";
+import { getCategories } from "../../services/api/CategoryApi";
+import { getBrands } from "../../services/api/BrandsApi";
+import RichTextEditor from "./RichTextEditor.jsx";
+import { FiUpload, FiTrash2 } from "react-icons/fi";
 
-const EditProduct = ({ onClose, refreshProducts, productId }) => {
-  const [product, setProduct] = useState(null); // Use null initially to handle loading state
-  const [loading, setLoading] = useState(true); // Loading state
+const EditProduct = ({ onClose, productId }) => {
+  const [formData, setFormData] = useState({
+    productName: "",
+    category: "",
+    brand: "",
+    description: "",
+    images: [],
+    attributes: [],
+    variants: [],
+  });
+  const [categories, setCategories] = useState([]);
+  const [brands, setBrands] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const editorRef = useRef();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const productData = await getProductById(productId);
-        setProduct(productData);
+        const [product, categoriesData, brandsData] = await Promise.all([
+          getProductById(productId),
+          getCategories(),
+          getBrands(),
+        ]);
+
+        setCategories(categoriesData);
+        setBrands(brandsData);
+
+        setFormData({
+          productName: product.name,
+          category: product.categoryId._id,
+          brand: product.brandId._id,
+          description: product.description,
+          images: product.imageUrls.map((url) => ({ file: null, url })),
+          attributes: product.attributes || [],
+          variants: product.variants.map((variant) => ({
+            ...variant,
+            price: variant.price || "",
+            quantity: variant.stockQuantity || "",
+          })),
+        });
       } catch (error) {
-        console.error("Error fetching product data:", error);
+        console.error("Error fetching product, categories, or brands:", error);
       } finally {
-        setLoading(false); // Loading complete
+        setLoading(false);
       }
     };
+
     fetchData();
-  }, [productId]); // Add productId as a dependency to re-fetch when it changes
+  }, [productId]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleDescriptionChange = (descriptionUrl) => {
+    setFormData({ ...formData, description: descriptionUrl });
+  };
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    const newImages = Array.from(files).map((file) => ({
+      file: file,
+      url: URL.createObjectURL(file),
+    }));
+
+    setFormData({
+      ...formData,
+      images: [...formData.images, ...newImages],
+    });
+  };
+
+  const handleImageDelete = (index) => {
+    const updatedImages = formData.images.filter((_, i) => i !== index);
+    setFormData({ ...formData, images: updatedImages });
+  };
+
+  const handleSetMainImage = (index) => {
+    const updatedImages = [...formData.images];
+    const mainImage = updatedImages.splice(index, 1)[0];
+    updatedImages.unshift(mainImage);
+    setFormData({ ...formData, images: updatedImages });
+  };
+
+  const handleAddAttribute = () => {
+    setFormData({
+      ...formData,
+      attributes: [...formData.attributes, { name: "", value: "" }],
+    });
+  };
+
+  const handleRemoveAttribute = (index) => {
+    const updatedAttributes = formData.attributes.filter((_, i) => i !== index);
+    setFormData({ ...formData, attributes: updatedAttributes });
+  };
+
+  const handleAttributeChange = (index, field, value) => {
+    const updatedAttributes = formData.attributes.map((attr, i) =>
+        i === index ? { ...attr, [field]: value } : attr
+    );
+    setFormData({ ...formData, attributes: updatedAttributes });
+  };
+
+  const handleAddVariant = () => {
+    setFormData({
+      ...formData,
+      variants: [
+        ...formData.variants,
+        {
+          attributes: [],
+          price: "",
+          quantity: "",
+        },
+      ],
+    });
+  };
+
+  const handleRemoveVariant = (index) => {
+    const updatedVariants = formData.variants.filter((_, i) => i !== index);
+    setFormData({ ...formData, variants: updatedVariants });
+  };
+
+  const handleVariantChange = (index, field, value) => {
+    const updatedVariants = formData.variants.map((variant, i) =>
+        i === index ? { ...variant, [field]: value || [] } : variant
+    );
+    setFormData({ ...formData, variants: updatedVariants });
+  };
+
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await editorRef.current.uploadToCloudinary();
+      await updateProduct(productId, formData);
+      alert("Product updated successfully");
+      onClose();
+    } catch (error) {
+      console.error("Error updating product:", error);
+      alert("Failed to update the product");
+    }
+  };
 
   if (loading) {
-    return <p>Loading product information...</p>;
-  }
-
-  if (!product) {
-    return <p>Product information could not be retrieved.</p>;
+    return (
+        <div className="bg-gray-700 p-6 rounded shadow-lg text-white">
+          <p>Loading product information...</p>
+        </div>
+    );
   }
 
   return (
-      <div>
-        <h2>Product Information</h2>
-        <ul>
-          <li><strong>ID:</strong> {product.id}</li>
-          <li><strong>Name:</strong> {product.name}</li>
-          <li><strong>Price:</strong> ${product.price}</li>
-          <li><strong>Category:</strong> {product.categoryId.name}</li>
-          <li><strong>Brand:</strong> {product.brandId.name}</li>
-          <li><strong>Description:</strong> {product.description}</li>
-        </ul>
-        <button onClick={onClose}>Close</button>
+      <div className="fixed top-0 inset-0 z-20 bg-black bg-opacity-50 py-6 px-4 sm:px-6 lg:px-8 overflow-auto">
+        <div className="w-[50%] mx-auto">
+          <form onSubmit={handleFormSubmit} className="bg-gray-900 shadow-md rounded-lg px-8 pt-4 pb-4">
+            <h2 className="text-2xl font-bold mb-2 text-white">Edit Product</h2>
+
+            {/* Product Name */}
+            <div className="mb-3">
+              <label htmlFor="productName" className="block text-gray-400 text-sm font-bold mb-2">
+                Product Name <span className="text-red-500 ml-1">*</span>
+              </label>
+              <input
+                  type="text"
+                  id="productName"
+                  name="productName"
+                  value={formData.productName}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
+
+            {/* Category */}
+            <div className="mb-3">
+              <label htmlFor="category" className="block text-gray-400 text-sm font-bold mb-2">
+                Category <span className="text-red-500 ml-1">*</span>
+              </label>
+              <select
+                  id="category"
+                  name="category"
+                  value={formData.category}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                <option value="">Select Category</option>
+                {categories.map((category) => (
+                    <option key={category._id} value={category._id}>
+                      {category.name}
+                    </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Brand */}
+            <div className="mb-3">
+              <label htmlFor="brand" className="block text-gray-400 text-sm font-bold mb-2">
+                Brand <span className="text-red-500 ml-1">*</span>
+              </label>
+              <select
+                  id="brand"
+                  name="brand"
+                  value={formData.brand}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              >
+                <option value="">Select Brand</option>
+                {brands.map((brand) => (
+                    <option key={brand._id} value={brand._id}>
+                      {brand.name}
+                    </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Description */}
+            <div className="mb-3">
+              <label htmlFor="description" className="block text-gray-400 text-sm font-bold mb-2">
+                Description
+              </label>
+              <RichTextEditor
+                  ref={editorRef}
+                  fileName={formData.productName}
+                  docxUrl={formData.description}
+                  onDescriptionUrlChange={handleDescriptionChange}
+                  className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  aria-label="Description"
+              />
+            </div>
+
+            {/* Attributes */}
+            <div className="mb-3">
+              <label className="block text-gray-400 text-sm font-bold mb-2">
+                Attributes
+              </label>
+              {formData.attributes.map((attribute, index) => (
+                  <div key={index} className="flex space-x-4 mb-2">
+                    <input
+                        type="text"
+                        value={attribute.name}
+                        onChange={(e) => handleAttributeChange(index, 'name', e.target.value)}
+                        placeholder="Attribute Name"
+                        className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md"
+                    />
+                    <input
+                        type="text"
+                        value={attribute.value}
+                        onChange={(e) => handleAttributeChange(index, 'value', e.target.value)}
+                        placeholder="Attribute Value"
+                        className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md"
+                    />
+                    <button
+                        type="button"
+                        onClick={() => handleRemoveAttribute(index)}
+                        className="bg-red-500 text-white p-2 rounded-full"
+                    >
+                      <FiTrash2 size={16} />
+                    </button>
+                  </div>
+              ))}
+              <button
+                  type="button"
+                  onClick={handleAddAttribute}
+                  className="text-blue-500 hover:underline"
+              >
+                Add Attribute
+              </button>
+            </div>
+
+            {/* Variants */}
+            <div className="mb-3">
+              <label className="block text-gray-400 text-sm font-bold mb-2">
+                Variants
+              </label>
+              {formData.variants.map((variant, index) => (
+                  <div key={index} className="flex space-x-4 mb-2">
+                    <input
+                        type="text"
+                        value={(variant.attributes && Array.isArray(variant.attributes) ? variant.attributes.join(", ") : "")}
+                        onChange={(e) =>
+                            handleVariantChange(index, 'attributes', e.target.value.split(", "))
+                        }
+                        placeholder="Variant Attributes"
+                        className="w-full px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md"
+                    />
+                    <input
+                        type="number"
+                        value={variant.price}
+                        onChange={(e) => handleVariantChange(index, 'price', e.target.value)}
+                        placeholder="Price"
+                        className="w-1/3 px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md"
+                    />
+                    <input
+                        type="number"
+                        value={variant.quantity}
+                        onChange={(e) => handleVariantChange(index, 'quantity', e.target.value)}
+                        placeholder="Quantity"
+                        className="w-1/3 px-3 py-2 bg-gray-700 text-gray-100 placeholder-gray-400 rounded-md"
+                    />
+                    <button
+                        type="button"
+                        onClick={() => handleRemoveVariant(index)}
+                        className="bg-red-500 text-white p-2 rounded-full"
+                    >
+                      <FiTrash2 size={16} />
+                    </button>
+                  </div>
+              ))}
+
+              <button
+                  type="button"
+                  onClick={handleAddVariant}
+                  className="text-blue-500 hover:underline"
+              >
+                Add Variant
+              </button>
+            </div>
+
+            {/* Images */}
+            <div className="mb-3">
+              <label htmlFor="images" className="block text-gray-400 text-sm font-bold mb-2">
+                Product Images
+              </label>
+              <div className="flex flex-col items-center">
+                {formData.images[0] && (
+                    <div className="relative mb-3">
+                      <img
+                          src={formData.images[0].url}
+                          alt="Main Image"
+                          className="h-48 w-48 object-cover rounded-lg border-4 border-indigo-500"
+                      />
+                      <button
+                          type="button"
+                          onClick={() => handleImageDelete(0)}
+                          className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1"
+                      >
+                        <FiTrash2 size={16} />
+                      </button>
+                    </div>
+                )}
+
+                {/* Image Thumbnails */}
+                <div className="grid grid-cols-3 gap-4">
+                  {formData.images.slice(1).map((image, index) => (
+                      <div key={index} className="relative">
+                        <img
+                            src={image.url || URL.createObjectURL(image.file)}
+                            alt={`Thumbnail ${index + 1}`}
+                            className="h-28 w-full object-cover border border-gray-300 rounded-md cursor-pointer"
+                            onClick={() => handleSetMainImage(index + 1)}
+                        />
+                        <button
+                            type="button"
+                            onClick={() => handleImageDelete(index + 1)}
+                            className="absolute top-2 right-2 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center text-xs"
+                        >
+                          ×
+                        </button>
+                      </div>
+                  ))}
+                </div>
+
+                <input
+                    type="file"
+                    id="images"
+                    name="images"
+                    accept="image/*"
+                    multiple
+                    onChange={handleFileChange}
+                    className="mt-3 hidden"
+                />
+                <label
+                    htmlFor="images"
+                    className="text-blue-500 cursor-pointer hover:underline flex items-center mt-2"
+                >
+                  <FiUpload size={16} />
+                  <span className="ml-2">Upload more images</span>
+                </label>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <button
+                  onClick={onClose}
+                  className="bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded mr-2"
+              >
+                Cancel
+              </button>
+              <button
+                  type="submit"
+                  className="px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Save Changes
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
   );
 };
--- src/pages/seller/product/ProductManage.jsx
+++ src/pages/seller/product/ProductManage.jsx
@@ -3,13 +3,8 @@ import { FaSearch } from "react-icons/fa";
 import DataTable, { createTheme } from "react-data-table-component";
 import AddProduct from "../../../components/products/AddProduct";
 import EditProduct from "../../../components/products/EditProduct";
-import {
-  deleteProduct,
-  getProductsByUserId,
-} from "../../../services/api/ProductApi";
+import {deleteProduct, getProductsByUserId,} from "../../../services/api/ProductApi";
 import {ToastContainer} from "react-toastify";
-// import { ToastContainer } from "react-toastify";
-
 createTheme(
   "dark",
   {
@@ -37,12 +32,13 @@ createTheme(
 );
 
 const ProductManage = () => {
-  // Open Modal
+  // State management
   const [isAddProductOpen, setIsAddProductOpen] = useState(false);
   const [isEditProductOpen, setIsEditProductOpen] = useState(false);
   const [products, setProducts] = useState([]);
   const [records, setRecords] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedProductId, setSelectedProductId] = useState(null); // State to store selected productId
 
   // Responsive Table
   const [scrollHeight, setScrollHeight] = useState("430px");
@@ -63,8 +59,8 @@ const ProductManage = () => {
       setLoading(true);
       const user = JSON.parse(localStorage.getItem("user"));
       const response = await getProductsByUserId(user.id);
-      setProducts(response); // Assuming the API response is an array of products
-      setRecords(response); // Initialize the records
+      setProducts(response);
+      setRecords(response);
     } catch (error) {
       console.error("Error fetching product data:", error);
     } finally {
@@ -73,39 +69,39 @@ const ProductManage = () => {
   };
 
   useEffect(() => {
-    updateScrollHeight(); // Cập nhật ngay khi component render lần đầu
-    window.addEventListener("resize", updateScrollHeight); // Lắng nghe sự kiện thay đổi kích thước
-    return () => window.removeEventListener("resize", updateScrollHeight); // Cleanup
+    updateScrollHeight();
+    window.addEventListener("resize", updateScrollHeight);
+    return () => window.removeEventListener("resize", updateScrollHeight);
   }, []);
 
-  // Fetch product data from API
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const user = JSON.parse(localStorage.getItem("user"));
-        const response = await getProductsByUserId(user.id);
-        console.log(`product data of seller ${user.id}: `,response);
-        setProducts(response); // Assuming the API response is an array of products
-        setRecords(response); // Initialize the records
-      } catch (error) {
-        console.error("Error fetching product data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
+    fetchProducts();
   }, []);
 
   const handleFilter = (event) => {
     const searchQuery = event.target.value.toLowerCase();
     const filteredProducts = products.filter((product) =>
-      product.name.toLowerCase().includes(searchQuery)
+        product.name.toLowerCase().includes(searchQuery)
     );
     setRecords(filteredProducts);
   };
 
-  // Fetch data
+  const handleDelete = async (row) => {
+    const confirm = window.confirm(
+        `Are you sure you want to delete ${row.name} with id ${row._id}?`
+    );
+    if (confirm) {
+      try {
+        const response = await deleteProduct(row._id);
+        alert(response.message);
+        await fetchProducts();
+      } catch (error) {
+        console.error("Error deleting product data:", error);
+      }
+    }
+  };
+
+  // Table columns
   const columns = [
     {
       name: "Name",
@@ -129,11 +125,11 @@ const ProductManage = () => {
       name: "Image",
       selector: (row) => row.imageUrls,
       cell: (row) => (
-        <img
-          src={row.imageUrls[0]}
-          alt={row.name}
-          className="w-10 h-10 object-cover rounded-md"
-        />
+          <img
+              src={row.imageUrls[0]}
+              alt={row.name}
+              className="w-10 h-10 object-cover rounded-md"
+          />
       ),
       sortable: false,
       center: "true",
@@ -142,103 +138,86 @@ const ProductManage = () => {
       name: "Action",
       center: "true",
       cell: (row) => (
-        <div className="max-md:flex max-md:w-56">
-          <button
-            className="bg-yellow-400 text-white px-2 py-1 rounded mr-2"
-            onClick={() => setIsEditProductOpen(true)}
-
-          >
-            Edit
-          </button>
-          <button
-            className="bg-red-500 text-white px-2 py-1 rounded"
-            onClick={() => handleDelete(row)}
-          >
-            Delete
-          </button>
-        </div>
+          <div className="max-md:flex max-md:w-56">
+            <button
+                className="bg-yellow-400 text-white px-2 py-1 rounded mr-2"
+                onClick={() => {
+                  setSelectedProductId(row._id); // Set selected productId
+                  setIsEditProductOpen(true);
+                }}
+            >
+              Edit
+            </button>
+            <button
+                className="bg-red-500 text-white px-2 py-1 rounded"
+                onClick={() => handleDelete(row)}
+            >
+              Delete
+            </button>
+          </div>
       ),
     },
   ];
 
-  // Button delete
-  const handleDelete = async (row) => {
-    const confirm = window.confirm(
-        `Are you sure you want to delete ${row.name} with id ${row._id}?`
-    );
-    if (confirm) {
-      try {
-        const response = await deleteProduct(row._id);
-        console.log(response);
-        alert(response.message);
-        await fetchProducts() // Add this line
-      }
-      catch (error) {
-        console.error("Error deleting product data:", error);
-      }
-    }
-  };
-
-  const handleFiter = (event) => {
-    const newData = data.filter((row) => {
-      return row.name.toLowerCase().includes(event.target.value.toLowerCase());
-    });
-    setRecords(newData);
-  };
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="h-screen">
-      <h1 className="grid place-items-center text-4xl py-4 text-white">
-        Manage Product
-      </h1>
-      <div className="w-[90%] lg:w-[70%] mx-auto rounded-md shadow-md">
-        <div className="flex justify-between my-2">
-          <button
-            className="btn-add bg-blue-500 text-white px-4 py-2 rounded"
-            onClick={() => setIsAddProductOpen(true)}
-          >
-            Add product
-          </button>
-          {/* Search Box */}
-          <div className="w-48 md:w-64 flex items-center rounded-md px-2 bg-gray-800">
-            <FaSearch className="flex items-center justify-center w-10 text-white" />
-            <input
-              type="text"
-              onChange={handleFiter}
-              placeholder="Search..."
-              className="bg-transparent w-44 border-none outline-none text-white focus:ring-0"
+      <div className="h-screen">
+        <h1 className="grid place-items-center text-4xl py-4 text-white">
+          Manage Product
+        </h1>
+        <div className="w-[90%] lg:w-[70%] mx-auto rounded-md shadow-md">
+          <div className="flex justify-between my-2">
+            <button
+                className="btn-add bg-blue-500 text-white px-4 py-2 rounded"
+                onClick={() => setIsAddProductOpen(true)}
+            >
+              Add product
+            </button>
+            {/* Search Box */}
+            <div className="w-48 md:w-64 flex items-center rounded-md px-2 bg-gray-800">
+              <FaSearch className="flex items-center justify-center w-10 text-white" />
+              <input
+                  type="text"
+                  onChange={handleFilter}
+                  placeholder="Search..."
+                  className="bg-transparent w-44 border-none outline-none text-white focus:ring-0"
+              />
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            <DataTable
+                theme="dark"
+                columns={columns}
+                data={records}
+                fixedHeader
+                pagination
+                fixedHeaderScrollHeight={scrollHeight}
+                paginationPosition="bottom"
             />
           </div>
         </div>
-        <div className="overflow-hidden">
-          <DataTable
-            theme="dark"
-            columns={columns}
-            data={records}
-            fixedHeader
-            pagination
-            fixedHeaderScrollHeight={scrollHeight}
-            paginationPosition="bottom"
-          />
-        </div>
+        {/* Modals */}
+        {isAddProductOpen && (
+            <AddProduct
+                onClose={() => setIsAddProductOpen(false)}
+                refreshProducts={fetchProducts}
+            />
+        )}
+        {isEditProductOpen && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+              <EditProduct
+                  onClose={() => setIsEditProductOpen(false)}
+                  refreshProducts={fetchProducts}
+                  productId={selectedProductId}
+              />
+            </div>
+        )}
+
+        <ToastContainer />
       </div>
-      {/* Modal */}
-      {isAddProductOpen && (
-        <AddProduct
-          onClose={() => setIsAddProductOpen(false)}
-          refreshProducts={fetchProducts} // Add this line
-        />
-      )}
-      {isEditProductOpen && (
-        <EditProduct
-            onClose={() => setIsEditProductOpen(false)}
-        />
-      )}
-      <ToastContainer />
-    </div>
   );
 };
 
